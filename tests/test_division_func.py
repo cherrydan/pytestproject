b941d84ae0c249da116241a5a38d49d07590a9aa
cherrydan <cherrydan.1978@gmail.com>
2022-11-17 23:17:09 +0300

    make parametrized tests

--- tests/test_division_func.py
+++ tests/test_division_func.py
@@ -1,6 +1,13 @@
+import pytest
+
+
 def division(a, b):
     return a / b
 
 
-def test_division_good():
-    assert division(10, 2) == 5
+@pytest.mark.parametrize("a, b, expected_result", [(10, 2, 5),
+                                                   (20, 10, 2),
+                                                   (30, -3, -10),
+                                                   (5, 2, 2.5)])
+def test_division_good(a, b, expected_result):
+    assert division(a, b) == expected_result

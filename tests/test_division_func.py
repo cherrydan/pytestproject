720952b93ec1cf4787ba2522956a7f15fc668580
cherrydan <cherrydan.1978@gmail.com>
2022-11-18 11:05:46 +0300

    make parametrized test fot errors

--- tests/test_division_func.py
+++ tests/test_division_func.py
@@ -9,3 +9,10 @@ from division import division
                                                    (5, 2, 2.5)])
 def test_division_good(a, b, expected_result):
     assert division(a, b) == expected_result
+
+
+@pytest.mark.parametrize("expected_exception, a, b", [(ZeroDivisionError, 10, 0),
+                                                      (TypeError, "10", "5")])
+def test_division_with_error(expected_exception, a, b):
+    with pytest.raises(expected_exception):
+        division(a, b)
